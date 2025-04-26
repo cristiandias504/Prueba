b079bf68f88117d6c67f61008271e0798cca072e
Cristian <cristian@gmail.com>
2025-04-25 22:37:39 -0500

    Version 6.2

---
+++ comandos-github.py
@@ -0,0 +1,7 @@
+# Ir a la carpeta donde tenemos guardado el proyecto
+
+# Comando para configurar y subir a github
+# git remote add origin git@github.com/cristiandias504/Prueba.git
+
+# Comando para subir un repositorio que se encuentra en local
+# git push -u origin main
